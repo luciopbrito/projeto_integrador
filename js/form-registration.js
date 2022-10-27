f8c8873c37fc095e6dfef92cc9d0bfd3346f10c1
Lúcio Brito <luciopereirabritoo@gmail.com>
2022-10-27 19:46:54 -0300

    commit

---
+++ js/form-registration.js
@@ -0,0 +1,15 @@
+// let enable = false;
+// const $form_registration = $('.form__registration');
+// const $form_login = $('.form__login');
+
+// $(document).on('click', '.form-login__cadastro', '.form-login-active', function () {
+//     if (enable == true) {
+//         $($form_login).css('display', 'none');
+//         $($form_registration).removeClass("disable").addClass("enable");
+//     } else {
+//         $($form_login).css('display', 'flex');
+//         $($form_registration).removeClass("enable").addClass("disable");
+//     }
+//     enable = !enable;
+//     console.log("foi!")
+// });
